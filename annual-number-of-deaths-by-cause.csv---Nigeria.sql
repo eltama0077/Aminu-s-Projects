72f8530a00aa1cfe6c162585d34f2207235186ad
eltama0077 <157919049+eltama0077@users.noreply.github.com>
2024-01-30 12:23:38 -0800

    Add files via upload

---
+++ annual-number-of-deaths-by-cause.csv---Nigeria.sql
@@ -0,0 +1,5 @@
+select * from annual_number_of_deaths_by_cause where ï»¿Entity like '%Nigeria';
+
+select Year,Deaths - Malaria from annual_number_of_deaths_by_cause where ï»¿Entity like '%Nigeria';
+
+
